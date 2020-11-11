823154485936ca77c8f6cece4789f658a66ce07d
Sponsoredby <74147554+Sponsoredby@users.noreply.github.com>
2020-11-11 10:16:56 +0000

    Add files via upload

---
+++ print x + text.py
@@ -0,0 +1,6 @@
+x = 2 #set the variable x 
+
+print (x, "is the value of the variable x") # print the variable x
+x = x + 2 #set the new variable for x 
+print() #prints a new line
+print (x, "...... is the result of x = x + 2") 
